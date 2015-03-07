54daa85b7b6d731acb9a5b9043dfad3714341590
root <root@debian>
2015-03-07 13:02:34 +0100

    final_update1

--- templates/footer.php
+++ templates/footer.php
@@ -5,6 +5,7 @@
  <footer>
 <center><p style="font-size: 11px;">Donate to Faucet: <?=$btclient->getaccountaddress($don_faucet);?> (recv: <?=$btclient->getbalance($don_faucet,0)?> BTCRY)</p>
 <center><p>Made By Greedi &copy; 2012</p>
+	<p>fixxed buggs and changed to bitcrystal faucet By Bitcrystal Developers &copy; 2015</p>
 </footer>
 </center>
 
